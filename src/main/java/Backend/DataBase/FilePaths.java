4f447deefbb99b6d3f5c40c3711fdf763ff25514
jomana <jomanaehab666@yahoo.com>
2024-12-13 13:41:43 +0200

    path changes

--- src/main/java/Backend/DataBase/FilePaths.java
+++ src/main/java/Backend/DataBase/FilePaths.java
@@ -10,8 +10,8 @@ package Backend.DataBase;
  * @author DELL-G3
  */
 public interface FilePaths {
-    String userDataBase = "src/Backend/DataBase/users.json";
-    String contentDataBase = "src/Backend/DataBase/content.json";
-    String requestsDataBase = "src/Backend/DataBase/friendRequests.json";
-    String groupsDataBase = "src/Backend/DataBase/groups.json";
+    String userDataBase = "src/main/java/Backend/DataBase/users.json";
+    String contentDataBase = "src/main/java/Backend/DataBase/content.json";
+    String requestsDataBase = "src/main/java/Backend/DataBase/friendRequests.json";
+    String groupsDataBase = "src/main/java/Backend/DataBase/groups.json";
 }
